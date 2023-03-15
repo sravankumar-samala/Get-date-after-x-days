9d9bfc77187c3305ebfac603e941e71e997eec2b
SravanKumar <srravankumarr@gmail.com>
2023-03-15 15:29:31 +0000

    modified months which will be printed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const addDays = require("date-fns/addDays");
 
 const daysFunction = (days) => {
   let result = addDays(new Date(2020, 06, 22), days);
-  return `${result.getDate()}-${result.getMonth()}-${result.getFullYear()}`;
+  return `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`;
 };
 
 module.exports = daysFunction;
